9372d50188fb1e5eafb3840f4668cb29d56c3168
Miraku-dev <85035112+Miraku-dev@users.noreply.github.com>
2021-06-02 00:22:08 +0300

    Delete bot directory

--- bot/utils.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher, types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-available_answers_data = ['да, всё верно.', 'нет, вернуться к вводу данных']
-
-class Location(StatesGroup):
-    wait_location = State()
-    await_location = State()
